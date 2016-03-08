2d76e94007b88be433141b655ad19295ca93a29f
noechavez0001 <noechavez0001@gmail.com>
2016-03-08 17:37:03 -0600

    Added files via upload
    SQL para la creacion de tablas de la base de datos

---
+++ creacion.de.tablas.sql
@@ -0,0 +1,12 @@
+﻿--cracion de la tabla usuario
+Create table usuario(Numero_cta int primary key, 
+					Nombre text, 
+					Apellido_Pat text, 
+					Apellido_Mat text);
+--creacion de la tabla dir que contiene los datos de la direccion del usuario
+Create table dir(Numero_cta int primary key references usuario(Numero_cta),
+				ncalle text, 
+				colonia text, 
+				cp text, 
+				del text, 
+				estado text) ;
